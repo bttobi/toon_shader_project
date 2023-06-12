c634967ef37a26491ae219ac934dea19ee40fa56
bttobi <bttobi123@gmail.com>
2023-06-12 20:55:41 +0200

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar fixed flex justify-center bg-base-100">
+      <p className="text-xl font-bold normal-case">Toon Shader Sandbox</p>
+    </nav>
+  );
+};
+
+export default Navbar;
