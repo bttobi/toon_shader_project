865c45e9b360ddbc4c581dfe50a28383d981e7ba
bttobi <bttobi123@gmail.com>
2023-06-13 20:19:25 +0200

    styling tweaks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="navbar fixed flex justify-center bg-base-100">
+    <nav className="navbar fixed z-30 flex justify-center bg-base-100">
       <p className="text-xl font-bold normal-case">Toon Shader Sandbox</p>
     </nav>
   );
